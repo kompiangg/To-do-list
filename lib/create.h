c3f2f4c0f417b6cdfea6cde8860dbc023ef6fa0d
Kompiang Gede Sukadharma <kompiang.sukadharma@gmail.com>
2021-05-23 00:09:16 +0800

    update/ add file operation in create session

--- lib/create.h
+++ lib/create.h
@@ -2,7 +2,9 @@
     #define __CREATE_
     #include "abstract_data_type.h"
     To_Do_List_Node *makeNode(To_Do_List_Node temp_input);
+    void initToDoListFromFile(To_Do_List_Node **main_node);
     void insert(To_Do_List_Node ***main_node, To_Do_List_Node temp_input);
     void helpAddProcess(int menu);
+    void saveToFile(To_Do_List_Node *temp_input);
     void addProcess(To_Do_List_Node **main_node);
 #endif
