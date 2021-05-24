11f826da5ef2be927568253fed129123756ee9e6
Agusarya592 <83224640+Agusarya592@users.noreply.github.com>
2021-05-24 09:49:39 +0800

    update-edit menu

--- main.c
+++ main.c
@@ -2,7 +2,20 @@
 #include <string.h>
 #include "lib/our_module.h"
 
-To_Do_List_Node *main_node = NULL;
+To_Do_List_Node *main_node = NULL, *temp;
+int edit_no;
+char temp_text_prioritas[5], temp_text_date[10], *token;
+
+void view(To_Do_List_Node *main_node){
+    int no_tugas = 1;
+    printf("\nIsi Data : \n");
+    while(main_node != NULL){
+        printf("\n%d. nama tugas : %s", no_tugas, main_node->nama_tugas);
+        main_node = main_node->next;
+        no_tugas++;
+    }
+    puts(" ");
+}
 
 int main() {
     int menu;
@@ -27,8 +40,17 @@ int main() {
             addProcess(&main_node, date_now);
         }
         else if (menu == 2) {
+            clear();
             // Tampilkan semua to-do list yang ada dengan urutan
+            view(main_node);
+
             // User akan memilih nomor tersebut
+            printf("\nTugas mana yang ingin Anda edit? \n>> ");
+            scanf("%d", &edit_no);
+
+            for(int i = 1; i<edit_no; ++i){
+                temp = temp->next;
+            }
             while (1) {
                 puts("===============");
                 puts("|  MENU EDIT  |");
@@ -56,16 +78,35 @@ int main() {
                         scanf("%d", &menu);
                         getchar();
                         if (menu == 1) {
-                            
+                            printf(" * Nama Tugas : ");
+                            scanf("%[^\n]", temp->nama_tugas);
+                            getchar();
                         }
                         else if (menu == 2) {
-
+                            printf(" * Kelompok Tugas : ");
+                            scanf("%[^\n]", temp->kelompok_tugas);
+                            getchar();
                         }
                         else if (menu == 3) {
-
+                            printf(" * Prioritas (1-4): ");
+                            scanf("%[^\n]", temp_text_prioritas);
+                            getchar();
+                            temp->prioritas = atoi(temp_text_prioritas);
+                            if (temp->prioritas < 1 && temp->prioritas > 4) {
+                                puts("\nMasukan angka antara 1 hingga 4\n");
+                            }
                         }
                         else if (menu == 4) {
+                            printf(" * Deadline (dd/mm/yyyy) : ");
+                            scanf("%[^%\n]", temp_text_date);
+                            getchar();
 
+                            token = strtok(temp_text_date, "/");
+                            temp->dl_dd = atoi(token);
+                            token = strtok(NULL, "/");
+                            temp->dl_mm = atoi(token);
+                            token = strtok(NULL, "/");
+                            temp->dl_yyyy = atoi(token);
                         }
                         else if (menu == 0) {
                             puts("\nPilih antara angka 0 hingga 2");
@@ -76,7 +117,17 @@ int main() {
                     }
                 }
                 else if (menu == 2) {
-
+                    //belum selesai
+                    if(strcmp(head->nim, find) == 0){
+                    del = head;
+                    head = head->next;
+                    head->prev = NULL;
+                    free(del);
+                } else (strcmp(del->nim, find) == 0){
+                    del->prev->next = del->next;
+                    del->next->prev = del->prev;
+                    free(del);
+                }
                 }
                 else if (menu == 0) {
                     break;
